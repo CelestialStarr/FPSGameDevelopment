6c0ae7d76f5783b24caa5219b2fcbc8b517d10cc
CelestialStarr <yu126040@gmail.com>
2025-07-27 22:52:44 +0800

    Health Pickup

--- Assets/Scripts/Weapon/WeaponManager.cs
+++ Assets/Scripts/Weapon/WeaponManager.cs
@@ -30,16 +30,6 @@ public class WeaponManager : MonoBehaviour
             }
         }
 
-        // Êó±ê¹öÂÖÇÐ»»ÎäÆ÷
-        float scrollInput = Input.GetAxis("Mouse ScrollWheel");
-        if (scrollInput > 0f)
-        {
-            SwitchWeapon((currentWeaponIndex + 1) % weapons.Length);
-        }
-        else if (scrollInput < 0f)
-        {
-            SwitchWeapon((currentWeaponIndex - 1 + weapons.Length) % weapons.Length);
-        }
     }
 
     public void SwitchWeapon(int newWeaponIndex)
